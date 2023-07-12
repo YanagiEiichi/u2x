ba355e17850e2b502c6384b6f3beb443210cddf8
YanagiEiichi <576398868@qq.com>
2023-07-12 22:17:17 +0800

    Install tsdoc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'eslint-plugin-tsdoc'],
   rules: {
     '@typescript-eslint/ban-types': [0],
   },
