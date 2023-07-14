1f3babed558f3a7abaea95e944c2a2180117b41f
YanagiEiichi <576398868@qq.com>
2023-07-14 14:31:18 +0800

    Remove ToArray

--- src/tests/u2a.test.ts
+++ src/tests/u2a.test.ts
@@ -1,5 +1,6 @@
 import { u2a, u2n } from '..';
 import { AssertTrue, IsEquals } from './libs';
+import { ToArray } from '../u2a';
 
 test('Basic u2a', () => {
   const r1 = u2a(1);
@@ -62,4 +63,22 @@ test('Never an array', () => {
   u2a(1, (v) => {
     true as AssertTrue<IsEquals<never, typeof v>>;
   });
+});
+
+test('ToArray', () => {
+  true as AssertTrue<IsEquals<1[], ToArray<1[]>>>;
+
+  true as AssertTrue<IsEquals<number[], ToArray<number[]>>>;
+
+  // Both `1[]` and `2[]` are array, and are kept.
+  true as AssertTrue<IsEquals<1[] | 2[], ToArray<1[] | 2[]>>>;
+
+  // A `undefined` is not an array, so it wil be ignored, and only `number[]` will be kept.
+  true as AssertTrue<IsEquals<number[], ToArray<undefined | number[]>>>;
+
+  // A `unknown` can be any type, including of course `unknown[]`, so an `unknown[]` should be returend.
+  true as AssertTrue<IsEquals<unknown[], ToArray<unknown | number[]>>>;
+
+  // A `1` is never an array, a `never` array should be returned.
+  true as AssertTrue<IsEquals<never[], ToArray<1>>>;
 });
